e02244bc21be6bb2665c842999a4553cf73fff4a
0xe <noemail>
2023-11-16 18:14:09 -0500

    Add tests for config defaults

---
+++ tests/test_config.py
@@ -0,0 +1,25 @@
+import pytest
+from config import Config, Device, load_config
+
+
+
+@pytest.fixture
+def config():
+    return load_config()
+
+
+# Ensure config values are default
+def test_config(config: Config):
+    assert config.log_path == "logs/checkmatrix.log"
+
+    assert config.model.path == "data/model.pth"
+    assert config.model.epochs == -1
+    assert config.model.device == Device.AUTO
+    assert config.model.learning_rate == 0.001
+    assert config.model.mcts.iterations == 300
+    assert config.model.mcts.workers == 6
+
+    assert config.stockfish.path == "stockfish.exe"
+    assert config.stockfish.depth == 10
+    assert config.stockfish.nodes == 100000
+    assert config.stockfish.time == 0.1
